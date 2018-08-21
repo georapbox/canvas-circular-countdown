ffd896e2af7f5c16f180ed95185dea47bf41df96
George Raptis <georapbox@gmail.com>
2018-08-21 12:04:42 +0300

    draw inner circle using canvas api, not css

---
+++ src/drawCanvas.js
@@ -0,0 +1,38 @@
+export default function drawCanvas(percentage, self) {
+  const { radius, barWidth, showPercentage } = self.options;
+  const circleStart = 1.5 * Math.PI;
+  const circleEnd = circleStart + percentage / 50 * Math.PI;
+  const centerX = self._canvas.width / 2;
+  const centerY = self._canvas.height / 2;
+
+  self._ctx.clearRect(0, 0, self._canvas.width, self._canvas.height);
+
+  // draw inner circle
+  self._ctx.beginPath();
+  self._ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  self._ctx.fillStyle = self.options.bgColor;
+  self._ctx.fill();
+
+  // draw empty bar
+  self._ctx.beginPath();
+  self._ctx.arc(centerX, centerY, radius - self.options.barOffset - self.options.barWidth / 2, circleStart, 4 * Math.PI, false);
+  self._ctx.lineWidth = barWidth;
+  self._ctx.strokeStyle = self.options.emptyBarBgColor;
+  self._ctx.stroke();
+
+  // draw filled bar
+  self._ctx.beginPath();
+  self._ctx.arc(centerX, centerY, radius - self.options.barOffset - self.options.barWidth / 2, circleStart, circleEnd, false);
+  self._ctx.lineWidth = barWidth;
+  self._ctx.strokeStyle = self.options.filledBarBgColor;
+  self._ctx.stroke();
+
+  // draw percentage
+  if (showPercentage) {
+    self._ctx.fillStyle = self.options.percentageFgColor;
+    self._ctx.font = `${self.options.percentageFontSize} ${self.options.percentageFontFamily}`;
+    self._ctx.textBaseline = 'middle';
+    self._ctx.textAlign = 'center';
+    self._ctx.fillText(`${Math.ceil(percentage)}%`, self._canvas.width / 2, self._canvas.height / 2);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Timer from '@georapbox/timer';
-import drawArc from './drawArc';
+import drawCanvas from './drawCanvas';
 import normalise from './utils/normalise';
 
 export default class CanvasCircularCountdown {
@@ -7,14 +7,14 @@ export default class CanvasCircularCountdown {
     const defaults = {
       duration: 10 * 1000,
       radius: 150,
-      arcWidth: 10,
-      arcOffset: 10,
+      barWidth: 10,
+      barOffset: 10,
       bgColor: '#ffffff',
-      emptyArcBgColor: '#dddddd',
-      filledArcBgColor: '#00bfeb',
+      emptyBarBgColor: '#dddddd',
+      filledBarBgColor: '#00bfeb',
       percentageFgColor: '#000000',
       percentageFontSize: '20px',
-      percentageFontFamily: 'Arial',
+      percentageFontFamily: 'sans-serif',
       showPercentage: true
     };
 
@@ -24,11 +24,6 @@ export default class CanvasCircularCountdown {
     }
 
     this.options = { ...defaults, ...options };
-
-    const canvasStyles = [
-      'border-radius: 50%;',
-      `background-color: ${this.options.bgColor};`
-    ].join(' ');
 
     if (element.nodeName === 'CANVAS') {
       this._canvas = element;
@@ -42,18 +37,13 @@ export default class CanvasCircularCountdown {
     this._timer = new Timer(this.options.duration, timer => {
       const percentage = normalise(timer.time().remaining, 0, this.options.duration) * 100;
       callback && callback(percentage, timer.time(), element);
-      drawArc(this, percentage);
+      drawCanvas(percentage, this);
     });
 
-    this.options.radius = this.options.radius - this.options.arcWidth / 2 - this.options.arcOffset / 2;
     this._ctx = this._canvas.getContext('2d');
-    this._canvas.width = this.options.radius * 2 + this.options.arcWidth + this.options.arcOffset;
-    this._canvas.height = this.options.radius * 2 + this.options.arcWidth + this.options.arcOffset;
-    this._canvas.style.cssText += canvasStyles;
-
-    drawArc(this, 100, {
-      remaining: this.options.duration
-    });
+    this._canvas.width = this.options.radius * 2;
+    this._canvas.height = this.options.radius * 2;
+    drawCanvas(100, this);
   }
 
   start(shouldReset = false) {
