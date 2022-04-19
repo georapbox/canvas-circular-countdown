19d3e53c756e0a9547084fccb894b229b39dea25
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2022-04-19 15:07:06 -0500

    Add support to elapsed time

--- src/timer.js
+++ src/timer.js
@@ -23,10 +23,10 @@ class Timer {
    * @param {Number} [duration] The timer's duration (ms). If left `undefined` or `0` or negative number the timer counts up instead of down.
    * @param {Function} [callback] Function to be executed while timer is running. The Timer instance is passed by as parameter.
    */
-  constructor(duration, callback) {
+  constructor(duration, elapsed, callback) {
     this._started = false;
     this._now = 0;
-    this._time = 0;
+    this._time = elapsed;
     this._duration = duration;
     this._callback = callback;
 
